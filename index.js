09cb1554ce3c53db84e6480bee088ebb401a3c39
Sleet <80127288+Sleet827@users.noreply.github.com>
2024-11-04 22:15:28 +0400

    Help Button Cube Ballz

--- index.js
+++ index.js
@@ -351,6 +351,7 @@ function toggleBalls() {
   if (ballsStyle) {
     ballsStyle.remove();
     document.title = "Cubes";
+    document.getElementById("helpBtnCubeBallz").innerHTML = "cubes";
   } else {
     let newStyle = document.createElement("link");
     newStyle.id = "ballsStyle";
@@ -358,6 +359,7 @@ function toggleBalls() {
     newStyle.href = "balls.css";
     document.head.appendChild(newStyle);
     document.title = "Balls";
+    document.getElementById("helpBtnCubeBallz").innerHTML = "balls";
   }
 }
 
